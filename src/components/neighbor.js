f39a46f837b96f3480f61e7ac63bcebd16242e13
kanakuii <naomidao814@gmail.com>
2024-09-29 05:00:02 -0500

    i did it mommy

--- src/components/neighbor.js
+++ src/components/neighbor.js
@@ -7,28 +7,16 @@ import neighbor from './assets/img/neighbor.png';
 // Associate each line with a speaker
 const storyData = [
   { text: "The neighbor, Theodore, lives next door to Detective Harper. Age: 56.", speaker: "neighbor" },
-  { text: "Sheriff Justin: Huff... huff... {PlayerName}! You must follow me quickly!", speaker: "sheriff"},
-  { text: "{PlayerName}: Officer Justin? What's wrong?", speaker: "sheriff" },
-  { text: "Sheriff Justin: There's no time to explain. Follow me!", speaker: "sheriff" },
-  { text: "You follow Sheriff Justin, anxious and confused. It isn't until you get to the scene that your eyes widen.", speaker: null },
-  { text: "{PlayerName}: ...", speaker: "player" },
-  { text: "{PlayerName}: ...?!?", speaker: "player" },
-  { text: "In front of you is the lifeless body of your mentor, who was alive just a few hours ago. The sight makes your blood run cold.", speaker: null },
-  { text: "{PlayerName}: How could this happen? What do you know? How long was she here for?!", speaker: "sheriff" },
-  { text: "Sheriff Justin: I'm sorry, {PlayerName}. I'm just as clueless as you are. The moment I saw this, I immediately ran to get you.", speaker: "sheriff" },
-  { text: "Detective Harper: I know this is hard... but you're the only one who can solve this case now. Will you help uncover this mystery?", speaker: "sheriff" },
-  { text: "... I can't believe it, but I know that I'm the only one in this town that knows what to do.", speaker: "player"},
-  { text: "It's up to me to avenge my mentor.", speaker: "player"},
-  { text: " ", speaker: null}
+  { text: "Sheriff Justin: Huff... huff... {PlayerName}! You must follow me quickly!", speaker: "neighbor"}
 ];
 
 // Map speakers to their images
 const characterImages = {
-  sheriff: sheriff,
+  neighbor: neighbor,
   player: null // No image for the player character
 };
 
-function neighbor() {
+function Neighbor() {
   const location = useLocation();
   const playerName = location.state?.playerName || ''; // Retrieve the player name from location state
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
@@ -76,4 +64,4 @@ function neighbor() {
   );
 }
 
-export default neighbor;
+export default Neighbor;
