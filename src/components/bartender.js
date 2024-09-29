499ffb2a6cac1aa5cb047d431809c718d9823cf1
MARCI VAN <mvan122004@gmail.com>
2024-09-29 09:43:32 -0500

    bartender

--- src/components/bartender.js
+++ src/components/bartender.js
@@ -5,9 +5,21 @@ import bartender from './assets/img/Bartender.png';
 
 // Associate each line with a speaker
 const storyData = [
-  { text: "The neighbor, Theodore, lives next door to Detective Harper. Age: 56.", speaker: "bartender" },
-  { text: "{PlayerName}: Howdy Neighbor! I’m investigating the murder of Detective Harper.", speaker: "bartender"},
-  { text: "Neighbor Theodore: Harper?! Dead?! This can’t be… she was alive just a few days ago…", speaker: "bartender" }
+  { text: "The bartender, Alexa, runs the town’s saloon and was in charge of hosting the party last night. Age: 26", speaker: null},
+  { text: "Bartender Alexa: Welcome back [user]! What brings you to my tavern so early in the day?", speaker: "bartender"},
+  { text: "{PlayerName}: This morning we found Detective Harper shot dead in the town. I need your cooperation in the investigation. ", speaker: null},
+  { text: "Bartender Alexa: No… It can’t be…", speaker: "bartender"},
+  { text: "{PlayerName}: Do you have any information?", speaker: null},
+  { text: "Bartender Alexa: You said she was shot? The only people in this town who own a gun are the Mayor and the Sheriff. The Sheriff always keeps his gun on him in case of emergencies.", speaker: "bartender"},
+  { text: "{PlayerName}: Who do you think it could be?", speaker: null},
+  { text: "Bartender Alexa: Hmm… now that I think about it… you know Theodore, the neighbor, right? Well last night, I could’ve sworn I heard the weirdest sounds from inside his house. It gave me a terrible feeling… ", speaker: "bartender"},
+  { text: "Bartender Alexa: I know the only people in this town that have a gun are the sheriff and the mayor, and the Sheriff always has his gun on him, if that’s useful. Other than that I’m not particularly sure, as I’m not super close to the town folk around here. You’re the first one I really talk with. ", speaker: "bartender"},
+  { text: "{PlayerName}: And why is that?", speaker: null},
+  { text: "Bartender Alexa: Well, I live pretty far from town, so I always try to hurry up so that I don't get home so late, rather than linger around. Last night even, I finished cleaning around 1 am and must’ve gotten home back around an hour later.", speaker: "bartender"}, 
+  { text: "{PlayerName}: Alright, well that accounts for your location. Was anyone there to see you?", speaker: null}, 
+  { text: "Bartender Alexa: No, but I was just cleaning up the tavern. All my staff had already left as they weren’t available for the party.", speaker: "bartender"}, 
+  { text: "{PlayerName}: Alright, thank you for your time.", speaker: null},
+  { text: "Bartender Alexa: Of course, I hope the truth comes to light.", speaker: "bartender"}
 ];
 
 const characterImages = {
