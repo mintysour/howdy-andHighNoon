dacef8df41c66830f2b7bed5c2295403aa892ba2
kanakuii <naomidao814@gmail.com>
2024-09-28 19:40:08 -0500

    yaeloo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { React, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Prologue from './Prologue';
 import './App.css';
+
 
 const buttonStyle = {
   backgroundColor: '#613624', 
@@ -12,32 +15,29 @@ const buttonStyle = {
 };
 
 function App() {
-  const[gameStarted, getGameStarted] = useState(false);
-
-  const handleStartGame = () => {
-    setGameStarted(true);
-  };
-
   return (
-      <div className="App">
-        <header className="App-header">
-        <h1>Howdy and High Noon:</h1>
-        <h2>A Western Whodunit </h2>
+    <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Howdy and High Noon:</h1>
+            <h2>A Western Whodunit</h2>
+            <Link to="/prologue">
+              <button style={buttonStyle}>
+                Start Game
+              </button>
+            </Link>
+          </header>
 
-       {/* Conditional rendering based on game state */}
-        {!gameStarted ? (
-          <button style={buttonStyle} onClick={handleStartGame}>
-            Start Game
-          </button>
-        ) : (
-          <div>
-            <h2>The game has started!</h2>
-            {/* Add more game content here */}
-
-          </div>
-        )}
-        </header>
-      </div>
-  );
+          {/* Define the Routes */}
+          <Switch>
+            <Route path="/prologue">
+              <Prologue />
+            </Route>
+            {/* Add more routes here for other parts of the game as needed */}
+          </Switch>
+        </div>
+      </Router>
+    );
 }
+
 export default App;
