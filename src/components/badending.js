f99628da71599574f2a17b08b324dfda7df1ac30
bhavana venkatesh <bvenkatesh2957@gmail.com>
2024-09-29 10:46:36 -0500

    LETS GOOOOO

--- src/components/badending.js
+++ src/components/badending.js
@@ -10,13 +10,14 @@ const storyData = [
   { text: "As you lock up the criminal, the town cheers your name.", speaker: null },
   { text: '"{PlayerName}! Thank you for saving us once again!"', speaker: null},
   { text: '"Safe once again thanks to {PlayerName}"', speaker: null},
-  { text: "The crowd rejoices, thankful that the loose murderer was now where they belong. Even though there is so much joy in your name, you can't help but feel wrong.", speaker:null},
-  { text: "You go back to Detective Harper's office, feeling lost and empty", speaker: null},
+  { text: "The crowd rejoices, thankful that the murderer on the loose was now where they belong. But even though the masses are rejoicing, you can't help but feel uneasy.", speaker:null},
+  { text: "You go back to Detective Harper's office, feeling confused and empty", speaker: null},
   { text: "As you look back at the familiar room, basking in the nostalgia, you hear the door open.", speaker: null},
-  { text: "Before you could turn around, you hear a loud bang and feel a sharp and stinging pain in your back", speaker:null},
-  { text: "???: I thought you really had me, but turns out, you're just as bad as a detective as that Harper was. "},
-  { text: "As your vision fades to black, you're filled with regret.", speaker:null},
-  { text: "You couldn't avenge Harper.", speaker:null}
+  { text: "Before you can turn around, you hear a loud bang and feel a sharp, stinging pain in your back", speaker:null},
+  { text: "???: I really thought you had me, but it turns out you're just as bad of a detective as that Harper was. Shouldn't have been poking your nose around in other people's business."},
+  { text: "As your vision fades to black, you're filled with sadness.", speaker:null},
+  { text: "You couldn't avenge Harper, and her murderer is still walking free. Maybe in another life...", speaker:null},
+  { text: ""}
 ];
 
 // Map speakers to their images
@@ -52,7 +53,7 @@ function Badending() {
         height: '100vh' // Full height
       }}
     >
-      {currentTextIndex < storyData.length - 1? (
+      {currentTextIndex < storyData.length -1 ? (
         // Render the dialogue box if we haven't reached the last index
         <>
           <p className="thetext">{currentText}</p>
@@ -66,8 +67,9 @@ function Badending() {
         // Render the mission box once the last line of dialogue is done
         <div className="text-box">
           <h2 className="mission">BAD ENDING</h2>
-          <p className="missiontext">You failed to catch the real culprit and let the real one get away! Try again, and read the clues carefully, detective!.</p>
-          //<p className="missiontext">Talk to the townspeople to uncover clues and complete your <b>TASKS</b> to fulfill your mission.</p>
+          <p className="missiontext">Oh no! You failed to catch the real culprit and let the real one get away! Try again, and read the clues carefully, detective!.</p>
+          <p className="missiontext">Talk to the townspeople again to uncover more clues and complete your <b>TASKS</b> to fulfill your mission.</p>
+          <p className="missiontext">CREDITS: Naomi Dao, Marci Van, Bhavana Venkatesh</p>
           <button className="missionButton" onClick={() => navigate('/')}>Restart?</button>
         </div>
       )}
