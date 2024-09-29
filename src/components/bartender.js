732432cf8ee65af3df5b0d350ed614314bd28f98
bhavana venkatesh <bvenkatesh2957@gmail.com>
2024-09-29 10:04:09 -0500

    what u want to type

--- src/components/bartender.js
+++ src/components/bartender.js
@@ -6,19 +6,19 @@ import bartender from './assets/img/Bartender.png';
 // Associate each line with a speaker
 const storyData = [
   { text: "The bartender, Alexa, runs the town’s saloon and was in charge of hosting the party last night. Age: 26", speaker: null},
-  { text: "Bartender Alexa: Welcome back [user]! What brings you to my tavern so early in the day?", speaker: "bartender"},
-  { text: "{PlayerName}: This morning we found Detective Harper shot dead in the town. I need your cooperation in the investigation. ", speaker: null},
+  { text: "Bartender Alexa: Welcome back {PlayerName}! What brings you to my tavern so early in the day?", speaker: "bartender"},
+  { text: "{PlayerName}: This morning we found Detective Harper shot dead in the town. I need your cooperation in the investigation. ", speaker: "bartender"},
   { text: "Bartender Alexa: No… It can’t be…", speaker: "bartender"},
-  { text: "{PlayerName}: Do you have any information?", speaker: null},
+  { text: "{PlayerName}: Do you have any information?", speaker: "bartender"},
   { text: "Bartender Alexa: You said she was shot? The only people in this town who own a gun are the Mayor and the Sheriff. The Sheriff always keeps his gun on him in case of emergencies.", speaker: "bartender"},
-  { text: "{PlayerName}: Who do you think it could be?", speaker: null},
+  { text: "{PlayerName}: Who do you think it could be?", speaker: "bartender"},
   { text: "Bartender Alexa: Hmm… now that I think about it… you know Theodore, the neighbor, right? Well last night, I could’ve sworn I heard the weirdest sounds from inside his house. It gave me a terrible feeling… ", speaker: "bartender"},
   { text: "Bartender Alexa: I know the only people in this town that have a gun are the sheriff and the mayor, and the Sheriff always has his gun on him, if that’s useful. Other than that I’m not particularly sure, as I’m not super close to the town folk around here. You’re the first one I really talk with. ", speaker: "bartender"},
-  { text: "{PlayerName}: And why is that?", speaker: null},
+  { text: "{PlayerName}: And why is that?", speaker: "bartender"},
   { text: "Bartender Alexa: Well, I live pretty far from town, so I always try to hurry up so that I don't get home so late, rather than linger around. Last night even, I finished cleaning around 1 am and must’ve gotten home back around an hour later.", speaker: "bartender"}, 
-  { text: "{PlayerName}: Alright, well that accounts for your location. Was anyone there to see you?", speaker: null}, 
+  { text: "{PlayerName}: Alright, well that accounts for your location. Was anyone there to see you?", speaker: "bartender"}, 
   { text: "Bartender Alexa: No, but I was just cleaning up the tavern. All my staff had already left as they weren’t available for the party.", speaker: "bartender"}, 
-  { text: "{PlayerName}: Alright, thank you for your time.", speaker: null},
+  { text: "{PlayerName}: Alright, thank you for your time.", speaker: "bartender"},
   { text: "Bartender Alexa: Of course, I hope the truth comes to light.", speaker: "bartender"}
 ];
 
--- src/components/sheriff.js
+++ src/components/sheriff.js
@@ -5,9 +5,21 @@ import sheriff from './assets/img/sherrif.png';
 
 // Associate each line with a speaker
 const storyData = [
-  { text: "The Sheriff is a tough but fair officer in charge of the investigation.", speaker: "sheriff" },
-  { text: "{PlayerName}: What can you tell me about the case?", speaker: "sheriff" },
-  { text: "Sheriff: We're doing everything we can. Harper had some shady dealings.", speaker: "sheriff" },
+  { text: "The Sheriff, Justin, works hard for a law-abiding and safe town. Age: 24.", speaker: null}, 
+  { text: "Sheriff Justin: Any progress on the investigation?", speaker: "sheriff"},
+  { text: "{PlayerName}: I have to keep that to myself, as unfortunately you are also a suspect sheriff.", speaker: "sheriff"}, 
+  { text: "Sheriff Justin: Ah, yes of course I understand.", speaker: "sheriff"},
+  { text: "{PlayerName}: Alright, so I have a couple questions for you, if that’s alright. Where were you last night?", speaker: "sheriff"},
+  { text: "Sheriff Justin: I stayed back after the party a little to use the restroom, and when I exited out of the tavern around 12, I saw that she was the last guest to leave, and with a real serious look on her face, like she was about to go somewhere important. I should’ve asked her what it was. Other than that, I was doing nightly patrols. Last night was particularly busy, since I had to escort many of the townsfolk to their homes.", speaker: "sheriff"},
+  { text: "{PlayerName}: Who do you think it could be?", speaker: "sheriff"},
+  { text: "Sheriff Justin: Well, to be honest, I’d been raising some sorts of suspicions towards the bartender, Alexa. She always seems like she’s in a rush to be somewhere, and she’s not really close with any of us townsfolk other than you.", speaker: "sheriff"},
+  { text: "{PlayerName}: Do you think I could see your gun? For inspection.", speaker: "sheriff"},
+  { text: "Sheriff Justin: Of course, {PlayerName}. I’ve got it right here.", speaker: "sheriff"},
+  { text: "The sheriff hands you his gun from his holster. It isn’t loaded, and it has his name engraved in the handle.", speaker: "sheriff"},
+  { text: "Sheriff Justin: My father got that for me when I got promoted to sheriff. It’s my prized possession.", speaker: "sheriff"},
+  { text: "You hand back that gun, and he puts it back in his holster.", speaker: "sheriff"}, 
+  { text: "{PlayerName}: Alright, thank you for your help.", speaker: "sheriff"},
+  { text: "Sheriff Justin: Of course, and good luck.", speaker: "sheriff"},
 ];
 
 const characterImages = {
